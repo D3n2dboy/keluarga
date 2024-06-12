7958e25e88d578b75c55fdf5f8542e512bbb7660
Ryazanov Denis <d3n2dboy@gmail.com>
2024-06-13 02:15:04 +0500

    add animations of scroll

--- src/js/modules/scripts.js
+++ src/js/modules/scripts.js
@@ -30,3 +30,45 @@ if (searchForemPlaceholder) {
         //searchForemPlaceholder.placeholder = 'Search form';
     }
 }
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length) {
+    window.addEventListener("scroll", animOmScroll);
+    function animOmScroll(params) {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_animation-start');
+            }
+            else {
+                if (!animItem.classList.contains("_animation-one-play")) {
+                    animItem.classList.remove('_animation-start');
+                }
+            }
+        }
+
+
+    }
+
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageXOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(animOmScroll, '300')
+    //animOmScroll();
+}
